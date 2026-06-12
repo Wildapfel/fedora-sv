e4963d8a34e3dc4178f7154a3ed49fa58e7949d5
Wildapfel <maxpetzold@yahoo.de>
2026-06-12 15:15:55 +0200

    tested

---
+++ ostree/ostree/rpm_ostree_packages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rpm-ostree install \
+    htop \
+    sushi \
+    fuse-sshfs \
+    crontabs \
+    docker \
+    powertop \
+    cava \
+    R \
+    R-devel \
+    'gcc-c++' \
+    java-25-openjdk \
+    make
